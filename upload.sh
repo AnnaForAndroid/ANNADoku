9e2294f9128b29c421915997ca3994924b3d31e9
Aaper <aram.parsegyan@gmail.com>
2017-04-07 14:32:46 +0200

    update

--- upload.sh
+++ upload.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+#create a new directory that will contain out generated pdf
 mkdir $HOME/buildPdf/
+#copy generated pdf from build folder to the folder just created
 cp -R ./ANNA/dokumentation.pdf $HOME/buildPdf/
 #go to home and setup git
 cd $HOME
 git config --global user.email "anna.studienarbeit@gmail.com"
 git config --global user.name "ProjectAnna"
+#clone the repository in the buildPdf folder
 git clone --quiet https://ProjectAnna:$GITHUB_API_KEY@github.com/AnnaForAndroid/ANNADoku
 #go into directory and copy data we're interested
 cd $HOME/ANNADoku
