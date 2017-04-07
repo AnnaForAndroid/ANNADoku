b833add9d8708376da8ba0e80d1c12734f064a55
Aaper <aram.parsegyan@gmail.com>
2017-04-07 14:32:39 +0200

    update

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-
-[![Build Status](https://travis-ci.org/AnnaForAndroid/ANNADoku.svg?branch=master)](https://travis-ci.org/AnnaForAndroid/ANNADoku)
 # ANNADoku
 
 Documentation for the ANNA project. Therefore checkout this link [ANNA](https://github.com/AnnaForAndroid/ANNA).
--- upload.sh
+++ upload.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+mkdir $HOME/buildPdf/
+cp -R ./ANNA/dokumentation.pdf $HOME/buildPdf/
+#go to home and setup git
+cd $HOME
 git config --global user.email "anna.studienarbeit@gmail.com"
 git config --global user.name "ProjectAnna"
-#clone the repository
 git clone --quiet https://ProjectAnna:$GITHUB_API_KEY@github.com/AnnaForAndroid/ANNADoku
-cp -Rf ./ANNA/dokumentation.pdf .
+#go into directory and copy data we're interested
+cd $HOME/ANNADoku
+cp -Rf $HOME/buildPdf/* .
 #add, commit and push files
 git add -f .
 git commit -m "[skip ci] Travis build $TRAVIS_BUILD_NUMBER pushed"
